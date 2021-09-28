72b080a682193ffa77da5e923f8788c659f24724
Sychov <artemkladchenkor@gmail.com>
2021-09-28 14:17:01 +0200

    1st try

---
+++ Shit.js
@@ -0,0 +1,13 @@
+let browser;
+if(browser === 'edge') {
+    alert ("You've got the Edge!");
+} else if (!(browser === 'Chrome'
+    || browser === 'Firefox'
+    || browser === 'Safari'
+    || browser === 'Opera')) {
+    alert('We hope that this page looks ok!')
+} else {
+    alert('Okey we support these browsers too')
+}
+
+
