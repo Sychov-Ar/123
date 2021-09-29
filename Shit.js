65998586587683b6ade8fa9fb903d0bcae0004a2
Sychov <artemkladchenkor@gmail.com>
2021-09-29 13:42:51 +0300

    ДЗ

--- Shit.js
+++ Shit.js
@@ -13,18 +13,42 @@
  //}
 
 
-// 2) переписать условия switch на if
+                        // 2) переписать условия switch на if
 
-const number = +prompt('Введите число между 0и3', '');
-switch (number) {
-    case 0:
-        alert('Вы ввели чило 0');
-        break;
-    case 1:
-        alert('вы ввели число 1');
-        break;
-    case 2:
-    case 3:
-        alert('Вы ввели чило 2 или 3')
-        break;
-}
+//const number = +prompt('Введите число между 0и3', '');
+//switch (number) {
+//    case 0:
+//        alert('Вы ввели чило 0');
+//        break;
+//    case 1:
+//        alert('вы ввели число 1');
+//        break;
+//    case 2:
+//    case 3:
+//        alert('Вы ввели чило 2 или 3')
+//        break;
+//}
+
+                     // 3 задание выводите четные числа
+
+//for(let i = 2; i<=10; i++) {
+//    if (i % 2 == 0) {
+//        alert(i)
+//    }
+//    }
+
+
+                     // 4) for на while
+//let i = 0
+//while (i<3) {
+//alert(`number${i}!`);
+//i++;
+//}
+                    // 5) повторять цикл пока воод не верный
+
+//let num;
+//do{ num = prompt('Введите число больше 100',0)
+//} while (num <=100 && num);
+
+
+
